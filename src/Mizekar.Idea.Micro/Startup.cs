b636b1020801cf1af6465459d7670c3d1133d8b9
Abolfazl <Abolfazl@Laptop>
2018-06-10 12:41:27 +0430

    no

--- src/Mizekar.Idea.Micro/Startup.cs
+++ src/Mizekar.Idea.Micro/Startup.cs
@@ -6,18 +6,23 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Mizekar.Idea.Micro.Data;
 
 namespace Mizekar.Idea.Micro
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly ILogger<Startup> _logger;
+
+        public Startup(IConfiguration configuration, ILogger<Startup> logger)
         {
             Configuration = configuration;
+            this._logger = logger;
         }
 
         public IConfiguration Configuration { get; }
@@ -25,6 +30,23 @@ namespace Mizekar.Idea.Micro
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var accountsDbConnection = Configuration["dbconnection"]; // docker config
+            if (accountsDbConnection == null)
+            {
+                accountsDbConnection = Configuration.GetConnectionString("dbconnection"); // app config
+            }
+
+            if (string.IsNullOrEmpty(accountsDbConnection))
+            {
+                services.AddDbContext<IdeaDbContext>(op => { op.UseInMemoryDatabase(Guid.NewGuid().ToString()); });
+                _logger.LogWarning("Database Use InMemory type");
+            }
+            else
+            {
+                services.AddDbContext<IdeaDbContext>(options => options.UseSqlServer(accountsDbConnection));
+                _logger.LogInformation("Database Use sql server");
+            }
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -42,6 +64,15 @@ namespace Mizekar.Idea.Micro
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<IdeaDbContext>();
+                if (!context.Database.IsInMemory())
+                {
+                    context.Database.Migrate();
+                }
+            }
         }
     }
 }
