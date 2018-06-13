a3aabb5cb74b2d40037b2153807ddc13f07eefeb
Abolfazl PC <Abolfazl.rajabpour@gmail.com>
2018-06-13 21:37:15 +0430

    1

---
+++ src/Mizekar.Idea.Micro/Data/Entities/IdeaSocialStatistic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Mizekar.Core.Data;
+
+namespace Mizekar.Idea.Micro.Data.Entities
+{
+    public class IdeaSocialStatistic : BusinessBaseEntity
+    {
+        public IdeaSocialStatistic()
+        {
+
+        }
+
+        public Guid IdeaInfoId { get; set; }
+        public virtual IdeaInfo IdeaInfo { get; set; }
+
+        public long LikeCount { get; set; }
+        public long ScoreSum { get; set; }
+        public long CommentCount { get; set; }
+        public long ViewCount { get; set; }
+    }
+}
