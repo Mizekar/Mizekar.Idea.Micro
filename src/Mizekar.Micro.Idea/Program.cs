b807b65f15f5d3f9110927aa62836ca36a8c69d6
Abolfazl <Abolfazl@Laptop>
2018-07-06 02:03:39 +0430

    serilog in appconfigs

--- src/Mizekar.Micro.Idea/Program.cs
+++ src/Mizekar.Micro.Idea/Program.cs
@@ -5,10 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 using Serilog;
-using Serilog.Events;
 
 namespace Mizekar.Micro.Idea
 {
@@ -16,19 +13,12 @@ namespace Mizekar.Micro.Idea
     {
         public static int Main(string[] args)
         {
-            // Get application base directory
-            string basedir = AppDomain.CurrentDomain.BaseDirectory;
-
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-                .Enrich.FromLogContext()
-                .WriteTo.File(basedir + "/logs/log-.txt", LogEventLevel.Debug, rollingInterval: RollingInterval.Day)
-                .CreateLogger();
             try
             {
                 Log.Information("Starting web host");
-                CreateWebHostBuilder(args).UseSerilog().Build().Run();
+                CreateWebHostBuilder(args)
+                    .UseSerilog((ctx, cfg) => cfg.ReadFrom.Configuration(ctx.Configuration))
+                    .Build().Run();
                 return 0;
             }
             catch (Exception ex)
